03026134571a8cc3400cb6e1c04cd103a55ddccb
Paolo4690 <paolo.calafiore.90@gmail.com>
2022-04-27 15:40:33 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('template.base')
+
+@section('title', 'DC Home')
+
+@section('content')
+    <h1>Home</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem!</p>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,49 @@
+<header>
+    <div class="line-blu">
+        <div class="container-line">
+            <ul>
+                <li><a href="#">DC POWER VISA</a></li>
+                <li><a href="#">ADDITIONAL DC SITES</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="container">
+        <div class="logo">
+        <img src="./images/dc-logo.png" alt="logo DC" />
+        </div>
+        <div class="header-menu">
+        <ul>
+            <li>
+                <a href="#">CHARACTERS</a>
+            </li>
+            <li class="active">
+                <a class="active" href="#">COMICS</a>
+            </li>
+            <li>
+                <a href="#">MOVIES</a>
+            </li>
+            <li>
+                <a href="#">TV</a>
+            </li>
+            <li>
+                <a href="#">GAMES</a>
+            </li>
+            <li>
+                <a href="#">COLLECTIBLES</a>
+            </li>
+            <li>
+                <a href="#">VIDEOS</a>
+            </li>
+            <li>
+                <a href="#">FANS</a>
+            </li>
+            <li>
+                <a href="#">NEWS</a>
+            </li>
+            <li>
+                <a href="#">SHOP</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+</header>
